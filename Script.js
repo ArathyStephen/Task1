0118e175dd8c33829a08cce44a9e985d0967932e
ArathyStephen <arathystephen432@gmail.com>
2025-07-17 11:41:43 +0530

    First push

---
+++ Script.js
@@ -0,0 +1,13 @@
+//git commmand
+
+//push...........
+
+//git init = git empty repo initialize
+
+//first time
+
+//git config --global user.email"arathystephen432@gmail.com"
+//git config --global user.name"ArathyStephen"
+//git add .  (particular file add script.js)
+
+//staging are : rdy to push to git hub
