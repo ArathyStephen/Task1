d887c19dac82a05d2b5bf2681cc41a86723ab1ee
ArathyStephen <arathystephen432@gmail.com>
2025-07-17 11:51:09 +0530

    Second Push

--- Script.js
+++ Script.js
@@ -1,3 +1,5 @@
+console.log('Arathy');
+
 //git commmand
 
 //push...........
@@ -8,6 +10,14 @@
 
 //git config --global user.email"arathystephen432@gmail.com"
 //git config --global user.name"ArathyStephen"
+
 //git add .  (particular file add script.js)
 
 //staging are : rdy to push to git hub
+
+//git commit -m "message"
+
+//git remote add origin https://github.com/ArathyStephen/Task1.git
+//git branch -M main
+
+//git push -u origin main
